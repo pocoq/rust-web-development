c9f91c82cf058a37f6ccac1fbab8c263d7aab065
pocoq <37747107+pocoq@users.noreply.github.com>
2023-06-16 08:58:45 +0700

    + add api: get, put, post, deleted question + add api: add answer for question

--- qa_service/src/main.rs
+++ qa_service/src/main.rs
@@ -1,8 +1,17 @@
+use serde::Serialize;
 use std::io::{Error, ErrorKind};
 use std::str::FromStr;
-use warp::Filter;
+use warp::{
+    filters::cors::CorsForbidden, http::Method, http::StatusCode, reject::Reject, Filter,
+    Rejection, Reply,
+};
 
 #[derive(Debug)]
+struct InvalidId;
+
+impl Reject for InvalidId {}
+
+#[derive(Debug, Serialize)]
 struct Question {
     id: QuestionId,
     title: String,
@@ -10,7 +19,7 @@ struct Question {
     tags: Option<Vec<String>>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Serialize)]
 struct QuestionId(String);
 
 impl Question {
@@ -35,18 +44,48 @@ impl FromStr for QuestionId {
     }
 }
 
-// fn main() {
-//     let question = Question::new(
-//         QuestionId::from_str("").expect("No id provided"),
-//         "First Question".to_string(),
-//         "Content of question".to_string(),
-//         Some(vec!["faq".to_string()]),
-//     );
-//     println!("{:?}", question);
-// }
-
 #[tokio::main]
 async fn main() {
-    let hello = warp::get().map(|| format!("Hello, world"));
-    warp::serve(hello).run(([127, 0, 0, 1], 3030)).await;
+    let cors = warp::cors()
+        .allow_any_origin()
+        .allow_header("not-in-the-request")
+        .allow_methods(&[Method::PUT, Method::DELETE, Method::GET, Method::POST]);
+    let get_items = warp::get()
+        .and(warp::path("questions"))
+        .and(warp::path::end())
+        .and_then(get_questions)
+        .recover(return_error);
+
+    let routes = get_items.with(cors);
+    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
+}
+
+async fn get_questions() -> Result<impl warp::Reply, warp::Rejection> {
+    let question = Question::new(
+        QuestionId::from_str("1").expect("No id provided"),
+        "First Question".to_string(),
+        "Content of question".to_string(),
+        Some(vec!["faq".to_string()]),
+    );
+    match question.id.0.parse::<i32>() {
+        Err(_) => Err(warp::reject::custom(InvalidId)),
+        Ok(_) => Ok(warp::reply::json(&question)),
+    }
+}
+
+async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
+    if let Some(error) = r.find::<CorsForbidden>() {
+        Ok(warp::reply::with_status(error.to_string(), StatusCode::FORBIDDEN))
+    }
+    else if let Some(InvalidId) = r.find() {
+        Ok(warp::reply::with_status(
+            "No valid ID presented".to_string(),
+            StatusCode::UNPROCESSABLE_ENTITY,
+        ))
+    } else {
+        Ok(warp::reply::with_status(
+            "Route not found".to_string(),
+            StatusCode::NOT_FOUND,
+        ))
+    }
 }
